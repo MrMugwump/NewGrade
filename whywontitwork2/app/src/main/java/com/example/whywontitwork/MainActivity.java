ce8f84ba518108af5fc013ddff16c3364d14f483
Jeeves <owencook1714@gmail.com>
2020-02-27 12:01:36 -0800

    Added watchers for lifecycle stages for testing

--- whywontitwork2/app/src/main/java/com/example/whywontitwork/MainActivity.java
+++ whywontitwork2/app/src/main/java/com/example/whywontitwork/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.CompoundButton;
 import android.widget.EditText;
@@ -55,6 +56,36 @@ public class MainActivity extends AppCompatActivity {
                         .apply();
             }
         });
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("lifecycle","onStart invoked");
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("lifecycle","onResume invoked");
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("lifecycle","onPause invoked");
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("lifecycle","onStop invoked");
+    }
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("lifecycle","onRestart invoked");
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("lifecycle","onDestroy invoked");
     }
 
     public void attemptToLogin(View view) {
